928c399d8fd76e62e6aabb843ae0b1261e19ba55
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-12-07 16:41:24 +0100

    need to check overrides carefully at home

--- default.nix
+++ default.nix
@@ -13,8 +13,9 @@ haskellPackages = super.haskellPackages.override {
     # vinyl = self.haskell.lib.dontCheck (hsNew.callPackage ./vinyl.nix {});
     vinyl = hsOld.vinyl_0_10_0 or hsOld.vinyl;
     megaparsec = hsOld.megaparsec_7_0_0 or hsOld.megaparsec;
-    SVGFonts = self.haskellPackages.callHackage "SVGFonts" "1.6.0.3" {};
+    # SVGFonts = (self.haskellPackages.callHackage "SVGFonts" "1.6.0.3" {}); # or hsOld.SVGFonts;
     # SVGFonts = (self.haskell.lib.packageSourceOverrides { SVGfonts = "1.6.0.3"; } self super).SVGfonts;
+    # time = hsOld.time_1_9_2 or hsOld.time;
 
     }; # overrides
   }; # override
