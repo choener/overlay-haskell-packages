2b69fbf6c2fdce717c2953c11c07b2ef316a1206
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-05-17 02:04:31 +0200

    hmatrix-backprop fun, and cleanup

--- default.nix
+++ default.nix
@@ -4,19 +4,20 @@ haskellPackages = super.haskellPackages.override {
   overrides = hsNew: hsOld: rec {
 
     # checks for this package fail
-    backprop = hsOld.backprop_0_2_6_1 or hsOld.backprop;
-    hmatrix-backprop = self.haskell.lib.dontCheck (hsOld.hmatrix-backprop_0_1_2_4 or hsOld.hmatrix-backprop);
-    hmatrix-vector-sized = self.haskell.lib.dontCheck hsOld.hmatrix-vector-sized;
+    # backprop = hsOld.backprop_0_2_6_1 or hsOld.backprop;
+    #hmatrix-backprop = self.haskell.lib.dontCheck (hsOld.hmatrix-backprop_0_1_2_4 or hsOld.hmatrix-backprop);
+    hmatrix-backprop = self.haskell.lib.dontHaddock (self.haskell.lib.dontCheck (hsNew.callHackage "hmatrix-backprop" "0.1.2.5" {}));
+    # hmatrix-vector-sized = self.haskell.lib.dontCheck hsOld.hmatrix-vector-sized;
     #zeromq4-haskell = self.haskell.lib.dontCheck hsOld.zeromq4-haskell;
     #zeromq4-haskell = self.haskellPackages.callHackage "zeromq4-haskell" "0.8.0" {};
 
     # add new versions of these packages
     #Frames = self.haskell.lib.dontCheck (hsNew.callPackage ./Frames.nix {});
     # vinyl = self.haskell.lib.dontCheck (hsNew.callPackage ./vinyl.nix {});
-    vinyl = hsOld.vinyl_0_10_0 or hsOld.vinyl;
-    megaparsec = hsOld.megaparsec_7_0_0 or hsOld.megaparsec;
-    semirings = hsNew.callPackage ./semirings.nix {};
-    Chart-diagrams = hsNew.callPackage ./Chart-diagrams.nix {}; # (self.haskellPackages.callHackage "Chart-diagrams" "1.9.2" {});
+    # vinyl = hsOld.vinyl_0_10_0 or hsOld.vinyl;
+    # megaparsec = hsOld.megaparsec_7_0_0 or hsOld.megaparsec;
+    # semirings = hsNew.callPackage ./semirings.nix {};
+    Chart-diagrams = hsNew.callPackage ./Chart-diagrams.nix {};
     Chart = hsNew.callPackage ./Chart.nix {};
 
     # SVGFonts = (self.haskellPackages.callHackage "SVGFonts" "1.6.0.3" {}); # or hsOld.SVGFonts;
